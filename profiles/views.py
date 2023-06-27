966e48751554a7463a02afcaf5a59c7551ddc6b4
Jonathan97-web <>
2023-06-27 22:31:27 +0000

    Fix typo

--- profiles/views.py
+++ profiles/views.py
@@ -9,7 +9,6 @@ from django.http import Http404
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        return Response(profiles)
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
 
