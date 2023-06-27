9203101dda50983a37f332b67c1c33412b3f9cf6
Jonathan97-web <>
2023-06-27 21:52:25 +0000

    Fix typos

--- socializer_api/settings.py
+++ socializer_api/settings.py
@@ -53,9 +53,9 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'rest_framework'
+    'rest_framework',
 
-    'profiles'
+    'profiles',
 ]
 
 MIDDLEWARE = [
