820cf3b68ae3ae976915612d43a6444398b072f6
Jonathan97-web <>
2023-06-28 02:19:28 +0000

    Fix typo and restructure

--- likes/models.py
+++ likes/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from posts.views import Post
+from posts.models import Post
 
 
 class Like(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='likes')
+        Post, related_name='likes', on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
