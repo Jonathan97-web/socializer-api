b68a92f10a27c64ddcb01cd81ea2923a686e06ee
Jonathan97-web <>
2023-06-28 02:36:50 +0000

    Add settings for JWTTokenauth and dj-rest-auth

--- socializer_api/settings.py
+++ socializer_api/settings.py
@@ -34,6 +34,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^0_bd-jet399sey+b4vo*s(b3hzyzj9f*n2z7kd@)m=j&6-ikw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+}
+
+REST_USE_JWT = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_SECURE = True
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'socializer_api.serializers.CurrentUserSerializer'}
+
+
 DEBUG = True
 
 ALLOWED_HOSTS = [
@@ -55,6 +72,16 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
+
+
+
 
     'profiles',
     'posts',
@@ -62,6 +89,8 @@ INSTALLED_APPS = [
     'likes',
     'followers',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
