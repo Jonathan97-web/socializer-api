db99e611316685f62296ea1f9c110cc2e0ea4134
Jonathan97-web <>
2023-07-02 22:34:36 +0000

    Edit notes model to use one-many relation

--- notes/models.py
+++ notes/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 
 class Notes(models.Model):
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.TextField(max_length=50)
     notes = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
